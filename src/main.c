24f905559d9b8fe03b128074e503f3ee0fcc32ad
Vladimir (np305) <nevada@live.ru>
2023-01-02 22:06:01 +0300

    Debug print moved to separate function

--- src/main.c
+++ src/main.c
@@ -4,36 +4,56 @@
 #include <string.h>
 #include <stdlib.h>
 
-
 datasegment_t* root;
 
-int main() {
-	datasegment_t* ptr;
 
-	IHEX_AppendHex(&root, "test.hex");
-
-	ptr = DATASEG_AutoAppend(&root);
-	DATASEG_Alloc(ptr, 10, 10);
-	ptr = DATASEG_AutoAppend(&root);
-	DATASEG_Alloc(ptr, 20, 50);
-
-	DATASEG_Fuse(root);
-
-	datasegment_t** arrX = DATASEG_Enumerate(root, 0);
-	// DATASEG_Remove(arrX[0]);
-	free(arrX);
-
-
+static void debug() {
 	uint32_t count = 555;
 	datasegment_t** arr = DATASEG_Enumerate(root, &count);
 
 	printf("List countains %d records:\n", count);
 	for (uint32_t i = 0; i < count; i++) {
 		// printf(" Record %d: %08x\n", i, (uint32_t) arr[i]);
-		printf(" Offset %08x, Length %d, Ptr %p\n", arr[i]->Offset, arr[i]->Length, arr[i]->Payload);
+		printf(" Offset %08x, Length %d\n", arr[i]->Offset, arr[i]->Length);
+
+		uint16_t left = arr[i]->Length;
+		uint8_t slen = 0;
+		uint32_t ii = 0;
+		while (left) {
+			if (!slen) {
+				printf("%08lx  ", arr[i]->Offset + ii);
+			}
+			printf(" %02X", *((uint8_t*)arr[i]->Payload+(ii++)));
+			if (slen++ == 15) {
+				slen = 0;
+				printf("\n");
+			}
+			left--;
+		}
+		printf("\n");
 	}
 	// printf(" Record %d: %08x\n", count, (uint32_t) arr[count]);
 	free(arr);
+}
+
+
+
+int main() {
+	datasegment_t* ptr;
+
+	IHEX_AppendHex(&root, "test.hex");
+	DATASEG_Fuse(root);
+	debug();
+
+	ptr = DATASEG_AutoAppend(&root);
+	DATASEG_Alloc(ptr, 10, 10);
+	memset(ptr->Payload, 0x11, ptr->Length);
+	// ptr = DATASEG_AutoAppend(&root);
+	// DATASEG_Alloc(ptr, 20, 50);
+
+	DATASEG_Fuse(root);
+	debug();
 
 	return 0;
 }
+
