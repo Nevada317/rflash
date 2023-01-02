ef2e9f31e8ced8faef1f1f1f83f4461a2fd45dfb
Vladimir (np305) <nevada@live.ru>
2023-01-02 16:38:44 +0300

    IHEX - parse types and prepare for data

--- ihex/ihex.c
+++ ihex/ihex.c
@@ -8,9 +8,121 @@
 
 #define MaxStringLength 4096
 
-static bool _try_to_add_seg(datasegment_t** ptr_root, char* line) {
-	(void)ptr_root;
-	printf("Line: %s\n", line);
+typedef struct {
+	uint32_t address_offset;
+	bool non_empty;
+	bool terminated;
+} ihex_context_t;
+
+typedef enum {
+	IHEX_Type_Data        = 0x00,
+	IHEX_Type_EOF         = 0x01,
+	IHEX_Type_ExtSegAddr  = 0x02,
+	IHEX_Type_StaSegAddr  = 0x03,
+	IHEX_Type_ExtLinAddr  = 0x04,
+	IHEX_Type_StaLinAddr   = 0x05
+} ihex_rtype_t;
+
+static uint16_t _read_hexBE(const char* str, uint8_t bytes) {
+	uint16_t temp = 0;
+	for (uint8_t i = 0; i < (2*bytes); i++) {
+		char ch = *(str+i);
+		temp <<= 4;
+		// Transition lowercase to uppercase
+		if (ch >= 0x60) ch -= 0x20;
+
+		if (ch < '0') {
+			// Skip step - below 0
+		} else if (ch <= '9') {
+			// Regular digit
+			temp |= ch - '0';
+		} else if (ch < 'A') {
+			// Skip step - not a letter
+		} else if (ch <= 'F') {
+			// HEX letter
+			temp |= ch - 'A' + 10;
+		}
+	}
+	return temp;
+}
+
+static bool _try_to_add_seg(datasegment_t** ptr_root, char* line, ihex_context_t* ctx) {
+	if (!ptr_root) return false;
+	if (!line) return false;
+
+	char* rptr = line;
+	if (*(rptr++) != ':') {
+		printf("SKIP: Ignored HEX line:\n%s\n", line);
+		return false;
+	}
+	if (strlen(rptr) < (2*(1+2+1+1))) {
+		printf("SKIP: Too short line:\n%s\n", line);
+		return false;
+	}
+
+	printf("\nParsing line:\n%s\n", line);
+
+	uint8_t payload_len = _read_hexBE(rptr, 1);
+	rptr += 2* 1;
+	printf("Length %02x, ", payload_len);
+
+	if ((int)strlen(line) != (2*(1+2+1+payload_len+1) + 1)) {
+		printf("SKIP: Bad line length:\n%s\n", line);
+		return false;
+	}
+
+	char* chptr = line+1;
+	uint8_t check = 0;
+	for (uint16_t i = 0; i < (1+2+1+payload_len+1); i++) {
+		check +=_read_hexBE(chptr, 1);
+		chptr += 2;
+	}
+	if (check) {
+		printf("SKIP: Checksum does not match:\n%s\n", line);
+		return false;
+	}
+
+	uint32_t addr = _read_hexBE(rptr, 2);
+	rptr += 2* 2;
+	printf("Addr %04x, ", addr);
+
+	ihex_rtype_t type = (ihex_rtype_t) _read_hexBE(rptr, 1);
+	switch (type) {
+		case IHEX_Type_Data:
+			// Only possible break
+			break;
+		case IHEX_Type_EOF:
+			if (payload_len != 0) {
+				printf("SKIP: Non-empty EOF:\n%s\n", line);
+				return false;
+			}
+			ctx->terminated = true;
+			return true;
+		case IHEX_Type_ExtSegAddr:
+			return false;
+			// break;
+		case IHEX_Type_StaSegAddr:
+			return false;
+			// break;
+		case IHEX_Type_ExtLinAddr:
+			return false;
+			// break;
+		case IHEX_Type_StaLinAddr:
+			return false;
+			// break;
+		default:
+			printf("Unknown record type %02x:\n%s\n", (uint8_t)type, line);
+			return false;
+	}
+	// Here we are if data line found
+	if (payload_len == 0) {
+		printf("SKIP: Empty data field:\n%s\n", line);
+		return false;
+	}
+
+
+
+
 	return false;
 }
 
@@ -21,7 +133,7 @@ bool IHEX_AppendHex(datasegment_t** ptr_root, char* filename) {
 		printf("Error opening file '%s'", filename);
 		return false;
 	}
-	bool hasSegments;
+	ihex_context_t ctx;
 	char str[MaxStringLength+1];
 	char* lfptr;
 	while (1) {
@@ -30,9 +142,16 @@ bool IHEX_AppendHex(datasegment_t** ptr_root, char* filename) {
 		// Try to read line
 		if (!fgets(str, MaxStringLength+1, fp)) break;
 		// Remove trailing newline
-		if (lfptr = strchr(str, '\n')) *lfptr = '\0';
+		if ((lfptr = strchr(str, '\n'))) *lfptr = '\0';
+		if ((lfptr = strchr(str, '\r'))) *lfptr = '\0';
+		// Remove GNU-style comments
+		if ((lfptr = strchr(str, '#'))) *lfptr = '\0';
+		// Remove trailing space
+		if ((lfptr = strchr(str, ' '))) *lfptr = '\0';
+		// Skip empty lines
+		if (!strlen(str)) continue;
 		// Parse line
-		if (_try_to_add_seg(ptr_root, str)) hasSegments = true;
+		_try_to_add_seg(ptr_root, str, &ctx);
 	}
-	return hasSegments;
+	return ctx.non_empty;
 }
