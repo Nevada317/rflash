b837d2675f2d6e55b020b97316d28d9e05a7d422
Vladimir (np305) <nevada@live.ru>
2023-01-14 15:18:07 +0300

    Add time to run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ shift
 ARGS="-c apu2 xxxx yyyy -p m162 -X -B 1200 -eU lfuse:w:0xff:m -Uhfuse:w:0xd7:m -U efuse:w:0xfb:m -U flash:w:test.hex:i -U flash:w:patch.hex:i"
 
 echo "Executing $EXE with args $ARGS"
-${EXE} ${ARGS}
+time ${EXE} ${ARGS}
 
 echo "Finished"
