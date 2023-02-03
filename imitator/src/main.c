f1d1ad0540600f48d7e3173593c919087974cdb0
Vladimir Taktakishvili <vladimir@taktakishvili.org>
2023-02-03 08:32:12 +0300

    Tick generates empty reports

--- imitator/src/main.c
+++ imitator/src/main.c
@@ -1,17 +1,25 @@
 #include <stdio.h>
 #include "tcp_server.h"
 #include "periodic.h"
+#include "rfp.h"
+
+#include <stdlib.h>
 
 periodic_timer_t* poll_timer;
 
 void my_cb(void* data, int length) {
 	printf("Rx cb: %d @ %p\n", length, data);
 	PERIODIC_TriggerTimer(poll_timer);
+	// server_send(data, length);
 	// server_send("test\n", 5);
 }
 
 void test_cb(void*) {
 	printf("Tick...\n");
+	rfp_buffer_t report = {0,};
+	rfp_flexbuffer_t* flex = RFP_CreateParcel(RFP_CMD_REPORT, 0, &report);
+	server_send(flex->Data, flex->Length);
+	free(flex);
 }
 
 int main(int argc, char *argv[]) {
